30500532748d8cd2c421a0c98bc9d6edec18291e
Chaobin Tang <ctang@redhat.com>
2013-05-27 14:42:56 +0800

    INIT: Initialized the project.

---
+++ tv.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Added by Chaobin Tang <cbtchn@gmail.com>
+from datetime import datetime
+
+import request
+
+
+class Schedule(object):
+    pass
+
+class Channels(dict):
+
+    def __init__(self):
+        # init with keys, and set all values to None
+        keys = self.keys()
+        dict.__init__(self, list(zip(keys, [None] * len(keys))))
+
+    def __getitem__(self, channel_name):
+        schedule = self[channel_name]
+        if item is None:
+            # get schedule
+            pass
+        return schedule
+
+    def __setitem__(self, channel_name, schedule):
+        if not isinstance(schedule, Schedule):
+            raise TypeError("Value must be an instance of Schedule")
+        self[channel_name] = schedule
+
+    def keys(self):
+        return range(5)
+        if self.keys():
+            return self.keys()
+        
